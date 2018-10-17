ffe24f12a4a76b9314b7cfe0ae7e2a56be4d53bb
Alexander Irribarra <airribarra@udec.cl>
2018-10-17 17:17:31 -0300

    interfaz

--- app/src/main/java/udec/telleo/MainActivity.java
+++ app/src/main/java/udec/telleo/MainActivity.java
@@ -1,5 +1,6 @@
 package udec.telleo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i = new Intent(this,ElegirViaje.class);
+        startActivity(i);
         Log.v("asdasd","afekjkhdfkfsdasdasdfsdfjhfkg");
     }
 }
