7111df7951356987c60d090fe148dda38c2798ed
Angelo <angelozapata@udec.cl>
2018-10-18 21:56:22 -0300

    layout

--- app/src/main/java/udec/telleo/MainActivity.java
+++ app/src/main/java/udec/telleo/MainActivity.java
@@ -1,7 +1,10 @@
 package udec.telleo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        //boton de mi wea
+        Button miboton = (Button) findViewById(R.id.miboton);
+        miboton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nueva = new Intent(MainActivity.this, crearviaje.class);
+                startActivity(nueva);
+            }
+        });
     }
 }
