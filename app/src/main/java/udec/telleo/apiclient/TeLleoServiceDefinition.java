6caf45c71748a86834338d6f994bb819cb1b9105
Angelo <angelozapata@udec.cl>
2018-10-21 19:27:54 -0300

    Agregada API

---
+++ app/src/main/java/udec/telleo/apiclient/TeLleoServiceDefinition.java
@@ -0,0 +1,33 @@
+package udec.telleo.apiclient;
+
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import udec.telleo.model.*;
+
+public interface TeLleoServiceDefinition {
+    @GET("conductores/{conductor}/viajes")
+    Call<List<Viaje>> getViajesDeConductor(@Path("conductor") String conductor);
+
+    @GET("viajes/{viajeid}/paradas")
+    Call<List<Parada>> getParadasDeViaje(@Path("viajeid")int idViaje);
+
+    @GET("viajes/{viajeid}")
+    Call<Viaje> getDatosViaje(@Path("viajeid")int idViaje);
+
+    @GET("conductores/{conductor}/viajes/{viajeid}/reservas")
+    Call<List<Reserva>> getReservasRecibidas(@Path("conductor")String conductor, @Path("viajeid") int idViaje);
+
+    @GET("viajes")
+    Call<List<Viaje>> getViajes(@Query("origen") String origen, @Query("destino") String destino,
+                                @Query("fechaminima") String fechamin, @Query("fechamaxima") String fechamax,
+                                @Query("asientos") int asientos, @Query("maletas") int maletas);
+    @POST("viajes/{viajeid}/pasajeros")
+    Call<ResponseBody> postReserva(@Body Reserva res, @Path("viajeid") int viajeid);
+}
--- app/src/main/java/udec/telleo/crearviaje.java
+++ app/src/main/java/udec/telleo/crearviaje.java
@@ -1,7 +1,13 @@
 package udec.telleo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class crearviaje extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class crearviaje extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crearviaje);
+        Button conf = (Button) findViewById(R.id.confirmar);
+        conf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Fecha: ",getFecha());
+                Log.d("Hora: ",getHora());
+                Log.d("Origen: ",getOrigen());
+                Log.d("Destino: ",getDestino());
+            }
+        });
+    }
+
+    private String getFecha()
+    {
+        return ((EditText)findViewById(R.id.textFecha)).getText().toString();
+    }
+    private String getHora()
+    {
+        return ((EditText)findViewById(R.id.textHora)).getText().toString();
+    }
+    private String getOrigen()
+    {
+        return ((EditText)findViewById(R.id.textOrigen)).getText().toString();
+    }
+    private String getDestino()
+    {
+        return ((EditText)findViewById(R.id.textDestino)).getText().toString();
     }
 }
