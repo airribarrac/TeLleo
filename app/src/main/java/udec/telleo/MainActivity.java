c13b7c5e36a60d8f893df078f03c9516de320655
Rodrigo Cordova Whiteley <r.a.c.whiteley@gmail.com>
2018-10-22 11:56:32 -0300

    merge desde irri

--- app/src/main/java/udec/telleo/MainActivity.java
+++ app/src/main/java/udec/telleo/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("asdasd","afekjkhdfkfsdasdasdfsdfjhfkg");
+    }
+
+    public void abrirElegirViaje(View view) {
         Intent i = new Intent(this,ElegirViaje.class);
         startActivity(i);
-        Log.v("asdasd","afekjkhdfkfsdasdasdfsdfjhfkg");
+
     }
 }
