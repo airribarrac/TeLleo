f14c2aa0c3799cba083a5927407ff6b378d21ccb
Alexander Irribarra <airribarra@udec.cl>
2018-11-05 22:10:16 -0300

    login y registro, falta integrar api

--- app/src/main/java/udec/telleo/Registro.java
+++ app/src/main/java/udec/telleo/Registro.java
@@ -3,19 +3,33 @@ package udec.telleo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class Registro extends AppCompatActivity {
-
+    private EditText usuario,mail,contra,contra2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        usuario = findViewById(R.id.userText);
+        mail = findViewById(R.id.mailText);
+        contra = findViewById(R.id.passwordText);
+        contra2 = findViewById(R.id.password2Text);
     }
     public void registrar(View v){
+        //validar si las weas están bien
+        if(!contra.getText().toString().equals(contra2.getText().toString())){
+            Log.v("password","no coinciden");
+            return;
+        }
         Intent intent = new Intent(this, Registro2.class);
-        intent.putExtra("username", "test1");
+        intent.putExtra("username", usuario.getText().toString());
+        intent.putExtra("mail",mail.getText().toString());
+        intent.putExtra("contra",contra.getText().toString());
         startActivity(intent);
-
     }
 }
--- app/src/main/java/udec/telleo/Registro2.java
+++ app/src/main/java/udec/telleo/Registro2.java
@@ -1,13 +1,31 @@
 package udec.telleo;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Registro2 extends AppCompatActivity {
-
+    private EditText nombre,apellido,rut,celular;
+    private String user,contra,mail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro2);
+        Intent i = getIntent();
+        user = i.getStringExtra("username");
+        mail = i.getStringExtra("mail");
+        contra = i.getStringExtra("contra");
     }
+
+    public void completar(View v){
+        //validar
+        DialogFragment pcd = new PreguntaConductorDialog();
+        //pcd.show(getSupportFragmentManager(),"alo");
+    }
+
 }
